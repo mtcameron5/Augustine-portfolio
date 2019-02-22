72fc5e12a9480e375c751111c670058c4f8c03c9
mtcameron5 <mtcameron5@gmail.com>
2019-02-22 08:11:51 -0700

    Implemented model defauls using before_initialize method

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,5 +5,12 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'Angular')
   end 
 
+  after_initialize :set_defaults 
+
+  def set_defaults 
+    self.main_image  ||= "https://placehold.it/600x400"
+    self.thumb_image ||= "https://placehold.it/350x200"
+  end 
+
 
 end
