5e432c47c6b4e1fbdb33d05204d82db5c588dae4
mtcameron5 <mtcameron5@gmail.com>
2019-02-27 04:18:06 -0700

    Implemented persistent position updates for drag and drop interface

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get 'angular-items', to: 'portfolios#angular'
   root to: 'pages#home'
 
-  resources :portfolios, except: [:show]
+  resources :portfolios, except: [:show] do 
+    put :sort, on: :collection
+  end 
+
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   resources :blogs do
