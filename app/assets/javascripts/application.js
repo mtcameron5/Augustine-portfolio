70b5a04c0b2ee6583a12c06bfc4aff03940ec1be
mtcameron5 <mtcameron5@gmail.com>
2019-02-27 02:46:00 -0700

    Installed jquery ui, sortable html5 library and css calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery3
+//= require jquery-ui
+//= require html.sortable
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
